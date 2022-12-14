211e715474827ef71088665d190ba98775ac1034
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-14 19:22:42 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
+
 const App = () => {
   //code here
   const [inputValue, setInputValue] = useState(0)
   return (
     <div id="main">
-      <ChildComponent />
+      Find The Square <br/>
       <input id='input' onChange={(event) => setInputValue(event.target.value)} />
-
-      <p id='output'>{Math.pow(inputValue, 2)}</p>
-    </div>
+      
+      <ChildComponent>
+      
+      <p id='output'>{Math.pow(inputValue,2)}</p>
+      </ChildComponent>
+     </div>
   )
 }
 
--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const ChildComponent = () => {
+const ChildComponent = (props) => {
 //code here
+ const pow = Math.pow(props.input,2);
   return (
     <div id="child">
-      Find The Square <br/>
      
+      {props.children}
     </div>
   )
 }
